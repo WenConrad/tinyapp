12786fadc62cc029d5097f08c108bb554f7ec2b8
WenConrad <conradwen@gmail.com>
2021-11-23 12:02:54 -0800

    lint helpers.js

--- helpers.js
+++ helpers.js
@@ -5,12 +5,12 @@ const { urlDatabase, users } = require('./server-data/database.js');
 const bcrypt = require('bcryptjs');
 
 const checkCookie = (cookie) => { //checks cookie session and returns data for use by other functions
-  if(users[cookie]) {
+  if (users[cookie]) {
     return {
       email: users[cookie].email, //used to filter database for shortURLs owned by user
       session: cookie, //used by header.ejs to render user session status
       notice: null, //used by header.ejs so that no alert banner wil be displayed
-    }
+    };
   }
   return {session: null, notice: null };
 };
@@ -23,14 +23,14 @@ const userURLs = (userID) => { //filter database to return list of shortURLs own
     }
   }
   return databaseUser;
-}
+};
 
 const checkUserAndPass = (req) => { //check database for user and password info, returns object for use in other functions
   const credentials = {
     userID: hashString(req.body.email), //will be assigned to generate a cookie
     userExists: false, //assigned default case of false
     passMatch: false, //assigned default case of false
-  }
+  };
   if (users[credentials.userID]) {
     credentials.userExists = true; //default case of false overwritten if user is in database
     credentials.passMatch = bcrypt.compareSync(req.body.password, users[credentials.userID].password); //if user exists check hash of password match, true or false assigned
